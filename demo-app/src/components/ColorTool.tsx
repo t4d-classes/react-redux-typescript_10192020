54fbf3135c9b71dd9896dca2ef6d8cf8f51163f5
Eric Greene <eric@t4d.io>
2020-10-19 16:30:49 -0400

    add code demos

--- demo-app/src/components/ColorTool.tsx
+++ demo-app/src/components/ColorTool.tsx
@@ -1,6 +1,37 @@
 import React from 'react';
 
+import { Color } from '../models/colors';
+
 export function ColorTool() {
+
+  const colors: Color[] = [
+    { id: 1, name: 'blue' },
+    { id: 2, name: 'purple' },
+    { id: 3, name: 'teal' },
+    { id: 4, name: 'fuschia' },
+    { id: 5, name: 'green' },
+  ];
+
+  // very imperative - both the what and the how
+
+  // const colorListItems: JSX.Element[] = [];
+
+  // traditional for-loop, there is no semantic meaning
+  // for (let x=0; x<colors.length; x++) {
+  //   const colorListItem = <li>{colors[x].name}</li>;
+
+  //   colorListItems.push(colorListItem);
+  // }
+
+  // more declarative approach
+  // iterating over a collection and performing a side-effect with each item
+  // colors.forEach((color) => {
+  //   const colorListItem = <li>{color.name}</li>;
+  //   colorListItems.push(colorListItem);
+  // });
+
+  // declarative and the semantic meaning is to transform one array in to another array
+  // const colorListItems = colors.map(color => <li>{color.name}</li>);
 
   return (
     <>
@@ -10,11 +41,7 @@ export function ColorTool() {
         </h1>
       </header>
       <ul>
-        <li>blue</li>
-        <li>purple</li>
-        <li>teal</li>
-        <li>fuschia</li>
-        <li>green</li>
+        {colors.map(color => <li key={color.id}>{color.name}</li>)}
       </ul>
     </>
   );
---
+++ demo-app/src/models/colors.ts
@@ -0,0 +1,10 @@
+// type alias
+export type Color = {
+  id: number;
+  name: string;
+};
+
+// export interface Color {
+//   id: number;
+//   name: string;
+// }
