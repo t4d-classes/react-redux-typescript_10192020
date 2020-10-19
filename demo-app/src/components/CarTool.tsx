dbaec91bf799802912662bbdacc9c092128d5d8e
Eric Greene <eric@t4d.io>
2020-10-19 14:27:08 -0400

    add solution exercise 2

--- demo-app/src/components/CarTool.tsx
+++ demo-app/src/components/CarTool.tsx
@@ -3,11 +3,43 @@ import React from 'react';
 export function CarTool() {
 
   return (
+    <>
     <header>
       <h1>
         Car Tool
       </h1>
     </header>
+    <table>
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Make</th>
+          <th>Model</th>
+          <th>Year</th>
+          <th>Color</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Ford</td>
+          <td>Fusion Hybrid</td>
+          <td>2018</td>
+          <td>red</td>
+          <td>45000</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Tesla</td>
+          <td>S</td>
+          <td>2019</td>
+          <td>blue</td>
+          <td>100000</td>
+        </tr>
+      </tbody>
+    </table>
+    </>
   );
 
 }
