a00827e72211850ec67f1ebbbd3ce1e22fd598bd
Eric Greene <eric@t4d.io>
2020-10-22 19:52:55 -0400

    add code demos

--- demo-app/src/index.tsx
+++ demo-app/src/index.tsx
@@ -1,14 +1,14 @@
-// default import
-import React from 'react';
-import { render } from 'react-dom';
-import { Provider } from 'react-redux';
+// // default import
+// import React from 'react';
+// import { render } from 'react-dom';
+// import { Provider } from 'react-redux';
 
-import { CalcToolContainer } from './containers/CalcToolContainer';
-import { calcToolStore } from './stores/calcToolStore';
+// import { CalcToolContainer } from './containers/CalcToolContainer';
+// import { calcToolStore } from './stores/calcToolStore';
 
-render(
-  <Provider store={calcToolStore}>
-    <CalcToolContainer />
-  </Provider>,
-  document.querySelector('#root'),
-);
+// render(
+//   <Provider store={calcToolStore}>
+//     <CalcToolContainer />
+//   </Provider>,
+//   document.querySelector('#root'),
+// );
